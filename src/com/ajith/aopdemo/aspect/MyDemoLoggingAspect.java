6f2fc0756aea0c5e4b576d06fa3c82ac4d9143b7
AJITH KUMAR A <12ajith1998@gmail.com>
2021-09-07 15:00:13 +0530

    Add files via upload

---
+++ src/com/ajith/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -0,0 +1,21 @@
+package com.ajith.aopdemo.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Order(2)
+public class MyDemoLoggingAspect {
+	
+	@Before("com.ajith.aopdemo.aspect.LuvAopExpressions.forDaoPackageNoGetterSetter()")
+	public void beforeAddAccountAdvice() {		
+		System.out.println("\n=====>>> Executing @Before advice on method");		
+	}
+	
+}
+
+
